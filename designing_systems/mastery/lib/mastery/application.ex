76a2632da354ce626c9ad1da1d69d921ebe917fc
rafaelbiten <rafaelbiten@icloud.com>
2020-12-03 20:24:35 -0500

    [FEAT] Add Registry and SessionSup to superv. tree

--- designing_systems/mastery/lib/mastery/application.ex
+++ designing_systems/mastery/lib/mastery/application.ex
@@ -11,7 +11,9 @@ defmodule Mastery.Application do
     children = [
       # Starts a worker by calling: Mastery.Worker.start_link(arg)
       # {Mastery.Worker, arg}
-      {Mastery.Boundary.QuizManager, [name: Mastery.Boundary.QuizManager]}
+      {Mastery.Boundary.QuizManager, [name: Mastery.Boundary.QuizManager]},
+      {Registry, [name: Mastery.Registry.QuizSession, keys: :unique]},
+      {DynamicSupervisor, [name: Mastery.Supervisor.QuizSession, strategy: :one_for_one]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
