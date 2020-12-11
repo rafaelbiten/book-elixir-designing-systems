ac58e180910e02f948c3edac8522a5c23a3de752
rafaelbiten <rafaelbiten@icloud.com>
2020-12-10 22:45:49 -0500

    [FEAT] API to work with the DB

--- designing_systems/mastery/mastery_persistence/lib/mastery_persistence.ex
+++ designing_systems/mastery/mastery_persistence/lib/mastery_persistence.ex
@@ -1,18 +1,39 @@
 defmodule MasteryPersistence do
-  @moduledoc """
-  Documentation for `MasteryPersistence`.
-  """
+  import Ecto.Query, only: [from: 2]
+  alias MasteryPersistence.{Response, Repo}
 
-  @doc """
-  Hello world.
+  def record_response(response, in_transaction \\ fn _response -> :ok end) do
+    {:ok, result} =
+      Repo.transaction(fn ->
+        %{
+          quiz_title: to_string(response.quiz_title),
+          template_name: to_string(response.template_name),
+          to: response.to,
+          email: response.email,
+          answer: response.answer,
+          correct: response.correct,
+          inserted_at: response.timestamp,
+          updated_at: response.timestamp
+        }
+        |> Response.record_changeset()
+        |> Repo.insert!()
 
-  ## Examples
+        in_transaction.(response)
+      end)
 
-      iex> MasteryPersistence.hello()
-      :world
+    result
+  end
 
-  """
-  def hello do
-    :world
+  def report(quiz_title) do
+    quiz_title = to_string(quiz_title)
+
+    from(
+      response in Response,
+      select: {response.email, count(response.id)},
+      where: response.quiz_title == ^quiz_title,
+      group_by: [response.quiz_title, response.email]
+    )
+    |> Repo.all()
+    |> Enum.into(Map.new())
   end
 end
