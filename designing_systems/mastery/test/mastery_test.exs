5092440b504c1564b937baa2f19133ac3dc6d897
rafaelbiten <rafaelbiten@icloud.com>
2020-12-22 19:53:19 -0500

    [SPEC] Stop persisting test data to DB

--- designing_systems/mastery/test/mastery_test.exs
+++ designing_systems/mastery/test/mastery_test.exs
@@ -62,6 +62,7 @@ defmodule MasteryTest do
 
   defp enable_persistence() do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
+    Ecto.Adapters.SQL.Sandbox.mode(MasteryPersistence.Repo, {:shared, self()})
   end
 
   defp response_count() do
--- designing_systems/mastery/test/test_helper.exs
+++ designing_systems/mastery/test/test_helper.exs
@@ -1,3 +1,4 @@
 Code.require_file("support/quiz_builders.exs", __DIR__)
 
 ExUnit.start()
+Ecto.Adapters.SQL.Sandbox.mode(MasteryPersistence.Repo, :manual)
